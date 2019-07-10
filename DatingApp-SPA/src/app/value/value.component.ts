b228e1adea6b0c02bacbcc334551dba2295cc78b
Jeff Weiersheuser <jweiersheuser@protocolgs.com>
2019-07-10 17:06:40 -0500

    Mid 3rd

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -1,8 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
-
-
 @Component({
   selector: 'app-value',
   templateUrl: './value.component.html',
